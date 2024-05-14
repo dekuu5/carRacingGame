cd737856fb4845c7abcedbfdac37091ee31204df
dekuu5 <deku332211@gmail.com>
2024-05-14 21:23:14 +0300

    first commit

---
+++ core/src/com/game/cargame/CarGame.java
@@ -0,0 +1,28 @@
+package com.game.cargame;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class CarGame extends Game {
+	public SpriteBatch batch;
+	public BitmapFont font;
+
+	public void create() {
+		batch = new SpriteBatch();
+		font = new BitmapFont(); // use libGDX's default Arial font
+		this.setScreen(new MainMenuScreen(this));
+	}
+
+	public void render() {
+		super.render(); // important!
+	}
+
+	public void dispose() {
+		batch.dispose();
+		font.dispose();
+	}
+}
---
+++ core/src/com/game/cargame/MainMenuScreen.java
@@ -0,0 +1,8 @@
+package com.game.cargame;
+
+import com.badlogic.gdx.Screen;
+
+public class MainMenuScreen implements Screen {
+    public MainMenuScreen(CarGame carGame) {
+    }
+}
