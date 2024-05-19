73f2484968eaa3e2d30b604ebf9626ee537b5e8e
alaa2702 <74565333+alaa2702@users.noreply.github.com>
2024-05-19 11:44:11 +0300

    split screen

--- core/src/com/game/cargame/GameScreen.java
+++ core/src/com/game/cargame/GameScreen.java
@@ -2,6 +2,7 @@ package com.game.cargame;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -14,8 +15,11 @@ import com.game.cargame.objects.Road;
 public class GameScreen implements Screen {
     final CarGame game;
     OrthographicCamera camera;
-    Car blueCar;
-    Car redCar;
+    Camera playerOneCamera;
+    Camera playerTwoCamera;
+
+    final Car blueCar;
+    final Car redCar;
     Car yellowCar;
     Thread blueCarThread;
     Thread redCarThread;
@@ -23,110 +27,112 @@ public class GameScreen implements Screen {
     Background bg;
     Road road;
     SpriteBatch batch;
-    ScreenViewport viewport;
+    ScreenViewport playerOneViewport;
+    ScreenViewport playerTwoViewport;
+
     public GameScreen(CarGame game) {
         this.game = game;
-        viewport = new ScreenViewport();
-        camera = (OrthographicCamera) viewport.getCamera();
 
+        camera = new OrthographicCamera();
         camera.setToOrtho(false, 1200, 1080);
-        camera.update();
+
+        playerOneCamera = new OrthographicCamera();
+        playerTwoCamera = new OrthographicCamera();
+
+        playerOneViewport = new ScreenViewport(playerOneCamera);
+        playerTwoViewport = new ScreenViewport(playerTwoCamera);
 
         blueCar = new Car("Blue Car", "car1.png", 100, 100, MovementType.WASD);
         redCar = new Car("Red Car", "car2.png", 500, 100, MovementType.RLUD);
-//        yellowCar = new Car("Yellow Car", "car3.png", 300, 100);
+
         bg = new Background();
         road = new Road();
         batch = game.batch;
+
         blueCarThread = new Thread(blueCar, "Blue Car Thread");
         redCarThread = new Thread(redCar, "Red Car Thread");
 
         blueCarThread.start();
         redCarThread.start();
-
     }
 
     @Override
     public void show() {
-
+        // Initialization if needed
     }
 
     @Override
     public void render(float delta) {
         ScreenUtils.clear(0, 0, 0, 1);
-//        camera.position.set(blueCar.getPosition(), 0);
-       updateCamerared();
-        camera.update();
-        batch.setProjectionMatrix(camera.combined);
 
-        updateAll();
+        updateAll(delta);
         renderAll();
-
     }
 
     @Override
     public void resize(int width, int height) {
-
+        playerOneViewport.update(width / 2, height);
+        playerTwoViewport.update(width / 2, height);
+        playerTwoViewport.setScreenX(width / 2);
     }
 
-    private void updateAll() {
-        road.update(Gdx.graphics.getDeltaTime(), camera.position.y, redCar.getSpeedY());
-
-        // Synchronize to ensure thread-safe update and retrieval of car positions
+    private void updateAll(float delta) {
         synchronized (blueCar) {
-            blueCar.update(Gdx.graphics.getDeltaTime());
+            blueCar.update(delta);
         }
         synchronized (redCar) {
-            redCar.update(Gdx.graphics.getDeltaTime());
+            redCar.update(delta);
         }
-       // bg.update(Gdx.graphics.getDeltaTime());
-
+        road.update(delta, playerOneCamera.position.y, blueCar.getSpeedY());
+        road.update(delta, playerTwoCamera.position.y, redCar.getSpeedY());
     }
 
     private void renderAll() {
+        updateCamera(playerOneCamera, blueCar);
+        batch.setProjectionMatrix(playerOneCamera.combined);
         batch.begin();
-        road.render(batch, camera.position.y);
-
-
+        road.render(batch, playerOneCamera.position.y);
         synchronized (blueCar) {
             blueCar.getSprite().draw(batch);
         }
+        batch.end();
+
+        updateCamera(playerTwoCamera, redCar);
+        batch.setProjectionMatrix(playerTwoCamera.combined);
+        batch.begin();
+        road.render(batch, playerTwoCamera.position.y);
         synchronized (redCar) {
             redCar.getSprite().draw(batch);
         }
         batch.end();
     }
-    private void updateCamerablue(){
-        camera.position.set(blueCar.getPosition().x + blueCar.getSprite().getWidth() / 2,
-                blueCar.getPosition().y + blueCar.getSprite().getHeight() / 2, 0);
-    }
-    private void updateCamerared() {
-      //  System.out.println("camera y : " + camera.position.y +"camera x" + camera.position.x );
 
-          camera.position.set(camera.position.x, redCar.getPosition().y + redCar.getSprite().getHeight() / 2, 0);
-
-    }
-
-
-    @Override
-    public void pause() { //handle pausing the game
-
+    private void updateCamera(Camera camera, Car car) {
+        camera.position.set(car.getPosition().x + car.getSprite().getWidth() / 2, car.getPosition().y + car.getSprite().getHeight() / 2, 0);
+        camera.update();
     }
 
     @Override
-    public void resume() { //handle resuming the game
+    public void pause() {
+        // Handle pause
+    }
 
+    @Override
+    public void resume() {
+        // Handle resume
     }
 
     @Override
     public void hide() {
-
+        // Handle hide
     }
 
     @Override
     public void dispose() {
         blueCar.dispose();
         redCar.dispose();
-        yellowCar.dispose();
+        if (yellowCar != null) {
+            yellowCar.dispose();
+        }
     }
 }
